a24f5170fdd6fd2d76da9055306da272dd6fc3b4
Elisandro Santolin <elisandro.santolin@outlook.com>
2024-04-26 19:58:02 -0300

    Refactor: implemented initial function callbacks

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hello node!')
+// console.log('Hello node!')
+
+// pega usuário e endereço
+function getUser(callback){
+    setTimeout(function (null, 
+    return {
+        name: 'John',
+        age: 20
+    }))
+}
+
+function getAddress(idUser){
+    setTimeout()
+    return {
+        city: 'Canada',
+        street: 'new flower tower',
+        number: '123'
+        
+    }
+
+}
+
+function getPhone(idUser){
+    setTimeout()
+    return {
+        ddd: '54',
+        phone: '1134454534'
+    }
+
+}
