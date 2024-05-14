a998c8e7f4f68c4f1db666f40a025450100ae690
Elisandro Santolin <elisandro.santolin@outlook.com>
2024-05-13 21:39:40 -0300

    Refactor: ajusted callbacks, necessary bugfix

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
-// console.log('Hello node!')
-
-// pega usuário e endereço
-function getUser(callback){
-   setTimeout(function (){
+// pega usuário
+function getUser(callback) {
+   setTimeout(function () {
         return callback (null, {
             id: 345,
             name: 'John',
             burn: new Date()    
             })
-    }, 2000)
- }
+    }, 1000);
+}
 
-function getAddress(idUser){
+function getAddress(idUser, callback) {
     setTimeout(()=> {
-
-    }) 
+        return callback (null, {
+            street: 'coast hunter',
+            region: 'west'
+            })
+        }, 2000);
 }
 
-function getPhone(idUser){
-    setTimeout()
-    return {
+function getPhone(idUser, callback) {
+    setTimeout(() => {
+    return callback (null, {
         ddd: '54',
-        phone: '1134454534'
-    }, 1000
+        phone: '991030295'
+        })
+    }, 2000);
 }
 
-const user = getUser;
-const address = getAddress;
-const phone = getPhone;
+function resolvedUser(erro, user) {
+    console.log('usuario' + user);
+}
 
+function resolvedPhone(error1, ) {
+    console.log('phone' + phoneUser);
+}
 
+getUser(function resolvedUser(error, user) {
+    //null || "" || 0 === false regra JS
+    if(error) {
+        console.error('Warning User!', error);
+        return;
+    }
+    getPhone(user.id, function resolvedPhone(error1, phone) {
+        if(error1) {
+            console.error('Warning Phone!', error1)
+            return;
+        }
+        getAddress(user.id, function resolvedAdress(error2, adress) {
+            if(error2) {
+                console.error('Warning Adress!', error2)
+                return;
+        }
+        
+        console.log(`
+           Nome: ${user.name},
+           Endereço: ${adress.street},
+           Telefone: ${phone.phone}
 
-// ##  setTimeout(function (null, 
-//     return {
-//         name: 'John',
-//         age: 20
-//     }, 1000}
+        `)
+        })
+    })
+});
